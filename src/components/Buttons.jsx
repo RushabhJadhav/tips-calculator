e3680e9a53dee401d90bc328321c05c1402d29df
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2024-03-09 12:52:36 +0530

    css changes

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,26 @@
+const Buttons = () => {
+    return (
+        <ul>
+            <li>
+                <input type="button" value="5%" />
+            </li>
+            <li>
+                <input type="button" value="10%" />
+            </li>
+            <li>
+                <input type="button" value="15%" />
+            </li>
+            <li>
+                <input type="button" value="25%" />
+            </li>
+            <li>
+                <input type="button" value="50%" />
+            </li>
+            <li>
+                <input type="number" placeholder="custom" />
+            </li>
+        </ul>
+    )
+}
+
+export default Buttons;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,24 @@
+import Buttons from "./Buttons";
+import Calculations from "./Calculations";
+
 const Form = () => {
     return (
         <div className="form-cont">
             <form>
                 <div className="bill">
-                    <label></label>
+                    <label>Bill</label>
+                    <input type="number" />
+                </div>
+                <div>
+                    <label>Select Tip %</label>
+                    <Buttons />
+                </div>
+                <div className="bill">
+                    <label>Number of People</label>
                     <input type="number" />
                 </div>
             </form>
+            <Calculations />
         </div>
     )
 }
