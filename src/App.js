46c00fa8bc0fedc1444225a8b5447b0d9eaa81f7
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2024-03-10 19:27:42 +0530

    context setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState, createContext } from "react";
 import Form from "./components/Form";
 
+const UserContext = createContext();
+
 function App() {
+
+  const [bill, setBill] = useState(0);
+  const [people, setPeople] = useState(0);
+  
   return (
-    <>
+    <UserContext.Provider value={{bill, setBill, people, setPeople}}>
       <Form />
-    </>
+    </UserContext.Provider>
   );
 }
 
